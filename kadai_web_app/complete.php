e91bbacdd7a4a3cfcce863868b36d538f613b514
maruyama-s1 <maruyamas.work@gmail.com>
2024-12-12 22:20:04 +0900

    kadai_web_appフォルダのアップロード

---
+++ kadai_web_app/complete.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP基礎編_課題</title>
+  </head>
+  <body>
+    <h2>登録が完了しました。</h2>
+    <p>社員情報がデータベースに保存されました。</p>
+    <button id="back_btn" onclick="location.href='form.php';">戻る</button>
+  </body>
+</html>
